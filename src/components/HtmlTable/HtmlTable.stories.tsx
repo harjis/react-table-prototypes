838859b27c81cab14b1b22477e2e7736cb15c3ea
Joonas Harjukallio <harjukallio@gmail.com>
2021-07-22 13:50:09 +0300

    Add more stories

--- src/components/HtmlTable/HtmlTable.stories.tsx
+++ src/components/HtmlTable/HtmlTable.stories.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Meta, Story } from '@storybook/react';
+import React from "react";
+import { Meta, Story } from "@storybook/react";
 
-import { Props as TableProps, HtmlTable } from './index';
-import { columns as defaultColumns, DummyData, getDummyData } from './helpers';
+import { Props as TableProps, HtmlTable } from "./index";
+import { columns as defaultColumns, DummyData, getDummyData } from "./helpers";
 
 export default {
-  title: 'HtmlTable',
+  title: "HtmlTable",
   component: HtmlTable,
 } as Meta;
 
@@ -13,8 +13,15 @@ const Template: Story<TableProps<DummyData>> = (args) => {
   return <HtmlTable {...args} />;
 };
 
-export const Basic = Template.bind({});
-Basic.args = {
+export const FitToContentWidth = Template.bind({});
+FitToContentWidth.args = {
   columns: defaultColumns,
   data: getDummyData(50),
+};
+
+export const FullWidth = Template.bind({});
+FullWidth.args = {
+  columns: defaultColumns,
+  data: getDummyData(50),
+  fullWidth: true,
 };
--- src/components/HtmlTable/components/HtmlTable/HtmlTable.tsx
+++ src/components/HtmlTable/components/HtmlTable/HtmlTable.tsx
@@ -1,24 +1,27 @@
-import React, { useMemo } from 'react';
-import { useTable } from 'react-table';
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+import cn from "classnames";
 
-import css from './HtmlTable.module.css';
+import css from "./HtmlTable.module.css";
 
-import { CustomColumn } from '../../types';
-import { customColumnsToReactTableColumns } from '../../utils/columnUtils';
-import { ColumnHeaders } from '../ColumnHeaders/ColumnHeaders';
-import { TableBody } from '../TableBody/TableBody';
+import { CustomColumn } from "../../types";
+import { customColumnsToReactTableColumns } from "../../utils/columnUtils";
+import { ColumnHeaders } from "../ColumnHeaders/ColumnHeaders";
+import { TableBody } from "../TableBody/TableBody";
 
 export type Props<Data extends Record<string, unknown>> = {
   columns: CustomColumn<Data>[];
   data: Data[];
+  fullWidth?: boolean;
 };
 
 export const HtmlTable = <Data extends Record<string, unknown>>(
-  props: Props<Data>,
+  props: Props<Data>
 ): JSX.Element => {
-  const columns = useMemo(() => props.columns.map(customColumnsToReactTableColumns), [
-    props.columns,
-  ]);
+  const columns = useMemo(
+    () => props.columns.map(customColumnsToReactTableColumns),
+    [props.columns]
+  );
   const data = useMemo(() => props.data, [props.data]);
   const tableInstance = useTable({
     columns,
@@ -26,7 +29,10 @@ export const HtmlTable = <Data extends Record<string, unknown>>(
   });
 
   return (
-    <table {...tableInstance.getTableProps()} className={css.Container}>
+    <table
+      {...tableInstance.getTableProps()}
+      className={cn(css.Container, { [css.FullWidth]: props.fullWidth })}
+    >
       <ColumnHeaders tableInstance={tableInstance} />
       <TableBody tableInstance={tableInstance} />
     </table>
